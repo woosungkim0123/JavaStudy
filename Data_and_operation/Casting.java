a0ffbfb9531aa9553b01bf7b25ad5b14fee09f9e
woosungkim0123 <sibu2005@naver.com>
2022-07-02 18:14:19 +0900

    java spring before study

---
+++ Data_and_operation/Casting.java
@@ -0,0 +1,19 @@
+
+public class Casting {
+
+	public static void main(String[] args) {
+		
+		double a = 1.1;
+		double b = 1;
+		// 정수지만 정수가 b라고 하는 변수에 담길때 더블형이라 자동으로
+		// 정수가 실수형으로 컨버팅이 됨
+		// 이렇게 컨버팅이 되면 잃어버리는 값이 없음
+		System.out.println(b);
+		
+		// int c = 1.1;
+		// 더블형인데 정수형에 담아서 오류
+		
+
+	}
+
+}
---
+++ Data_and_operation/Letter.java
@@ -0,0 +1,10 @@
+
+public class Letter {
+
+	public static void main(String[] args) {
+		String name = "Woosung";
+		System.out.println("Hello, " + name + " ... " + name + " ... bye");
+
+	}
+
+}
