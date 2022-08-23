a59d9c0f90ebe6eb1bfaa98ecc59b93c5cc4fe38
woosungkim0123 <sibu2005@naver.com>
2022-08-23 17:52:16 +0900

    포함, 다중 상속, 단일 상속, Object 클래스

--- 05_OOP_2/A_Inheritance.java
+++ 05_OOP_2/A_Inheritance.java
@@ -6,6 +6,37 @@ public class A_Inheritance {
         두 클래스를 부모와 자식으로 관계를 맺어주는 것
         자손은 조상의 모든 멤버를 상속받는다.(생성자, 초기화블럭 제외) => 자손의 멤버 개수는 조상보다 적을 수 없다(같거나 많다)
         자손의 변경은 조상에 영향을 미치지 않는다.
+
+
+        단일 상속(Single Inheritance)
+        자바는 단일상속만을 허용(C++은 다중상속 허용)
+        하나의 부모만 상속
+        class TvDVD extends TV, DVD {} // 에러
+
+        다중상속의 문제점
+        부모가 두개일때 이름이 동일하지만 내용은 다른 메서드가 있으면 어떤 것을 상속받아야 하는지 충돌 문제가 있음
+
+        참고 : 인터페이스를 이용하여 충돌문제를 해결하면서 다중 상속과 같은 효과를 낼 수 있음
+
+        인터페이스를 사용하지않고 다중상속처럼 편리하게 작성하는 방법
+        하나만 상속관계로 하고 나머지는 포함관계로 하는 것 => Class TvDVD
+        비중이 높은 클래스 하나만 상속관계, 나머지는 포함관계
+        
+        Object 클래스 - 모든 클래스의 조상
+        부모가 없는 클래스는 자동적으로 Object 클래스를 상속받게 된다.
+
+        class Tv {
+            boolean power;
+        }
+
+        컴파일러가 컴파일 시 자동으로 추가
+
+        class Tv extends Object {
+            boolean power;
+        }
+
+        모든 클래스는 Object 클래스에 정의된 11개의 메서드를 상속받는다.(toString(), equals(Object obj), hashCode(), ...)
+        
         */
 
     }
@@ -34,4 +65,24 @@ class Point3D1 {
 // Point3D2 클래스는 Point 클래스와 관게가 있어서 Point 클래스의 변경에 영향을 받음
 class Point3D2 extends Point {
     int z;
+}
+
+class Tv {
+    boolean power;
+    int channel;
+
+    void power() { power = !power; }
+}
+class DVD {
+    boolean power;
+
+    void power() { power = !power; }
+}
+class TvDVD extends Tv {
+    DVD dvd = new DVD();
+
+    void play() {
+        // 메서드를 만들어 줘야하는 번거로움이 있긴 하지만 안에 코드를 작성할 필요 없이 호출해서 사용하면 됨
+        dvd.power();
+    }
 }
