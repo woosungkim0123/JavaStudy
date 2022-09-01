b275eccbe852671c6e0311bca2df52f2b53ef735
woosungkim0123 <sibu2005@naver.com>
2022-09-01 22:09:40 +0900

    매개변수 다형성, 하나의 배열로 여러 종류 객체 저장

--- 05_OOP_2/L_Polymorphism2.java
+++ 05_OOP_2/L_Polymorphism2.java
@@ -12,7 +12,7 @@ public class L_Polymorphism2 {
         1. 다형적 매개변수
         2. 하나의 배열로 여러 종류 객체 다루기
 
-        매개변수의 다형성
+        1. 매개변수의 다형성
 
         매개변수가 참조형이면 메서드 호출시 자신과 같은 타입 또는 자손타입의 인스턴스를 넘겨줄 수 있다. => Buyer
         */
@@ -20,6 +20,25 @@ public class L_Polymorphism2 {
         Product p = new Product();
         // 참고 (두개 같음)
         System.out.println(p + "를 구매하셨습니다"); // System.out.println(p.toString + "를 구매하셨습니다");
+
+
+        /*
+        2. 하나의 배열에 여러 종류 객체 저장
+
+        조상타입의 배열에 자손들의 객체를 담을 수 있다.
+        */
+        Product p1[] = new Product[3];
+        p1[0] = new Tv1();
+        p1[1] = new Computer();
+        p1[2] = new Audio();
+
+        /*
+        Vector 클래스에는 Object 배열을 가지고 있어서 모든 종류 객체 저장 가능
+        배열의 단점 => 한번 만들면 크기 변경 불가능 => 크기가 모자라면 새로운 배열 만들고 복사하고 다 관리 해야하지만 벡터 클래스는
+        자기가 다 알아서 해줌 => 신경 쓸 필요가 없음(add 메서드로 추가만 해주면 됨)
+
+        */
+
     }
 }
 
